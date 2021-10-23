95c36013af2ca8081621fd0b7c5c1dc11e1af4a8
CarlosSaz22 <61812538+CarlosSaz22@users.noreply.github.com>
2021-10-22 19:50:26 -0600

    Se agrega la segunda parte del procedimiento

---
+++ guia-9-pt2/markers.js
@@ -0,0 +1,41 @@
+export const markers = [
+  {
+    coordinate: {
+      latitude: 13.4945688,
+      longitude: -89.3868631,
+    },
+    title: 'Playa el Tunco',
+    description: 'Playa el Tunco en la Libertad',
+    image:
+      'https://imgs-akamai.mnstatic.com/bb/95/bb95475cbbbba206ddac6ef12a8f5984.jpg',
+  },
+  {
+    coordinate: {
+      latitude: 13.1698526,
+      longitude: -88.3076309,
+    },
+    title: 'Playa El Espino',
+    description: 'Playa El Espino Usulután',
+    image:
+      'https://cdn-pro.elsalvador.com/wp-content/uploads/2018/01/El-Espino.jpg',
+  },
+  {
+    coordinate: {
+      latitude: 13.4924991,
+      longitude: -89.409454,
+    },
+    title: 'Playa el Sunzal',
+    description: 'Playa el Sunzal La Libertad',
+    image: 'https://turismo.sv/wp-content/uploads/2019/06/el-sunzal-6.jpg',
+  },
+  {
+    coordinate: {
+      latitude: 13.5249991,
+      longitude: -89.8141763,
+    },
+    title: 'Playa los Cóbanos',
+    description: 'Playa los cóbanos Sonsonate',
+    image:
+      'https://www.viajeroselsalvador.com/uploads/5/6/1/0/5610753/9356242_orig.jpg',
+  },
+];
